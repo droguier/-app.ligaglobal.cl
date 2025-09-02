5b415eea60275875863d7cafc5679a3f3a872047
Daniel Droguier <echoes.daniel@gmail.com>
2025-09-01 22:01:48 -0400

    mejora control google
    mejora control google

--- app.ligaglobal.cl/app/Http/Controllers/Auth/GoogleController.php
+++ app.ligaglobal.cl/app/Http/Controllers/Auth/GoogleController.php
@@ -8,6 +8,8 @@ use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
+use function Laravel\Prompts\error;
+
 class GoogleController extends Controller
 {
     /**
@@ -18,30 +20,22 @@ class GoogleController extends Controller
         $dummyUser = [
             'id' => '1',
             'name' => 'Usuario Dummy Echoes',
-            'email' => 'echoes@echoes.cl',
+            'email' => 'echoes@gmail.com',
             'avatar' => 'https://i.pravatar.cc/150?img=3',
             'google_id' => '1234567890',
         ];
-        $user = User::firstOrCreate([
-            'email' => $dummyUser['email'],
-        ], [
-            'name' => $dummyUser['name'],
-            'google_id' => $dummyUser['google_id'],
-            'avatar' => $dummyUser['avatar'],
-            'password' => bcrypt('dummy_password')
-            //'is_admin' => false
-        ]);
 
-        // Validar si el usuario es administrador y está activo
-        //$isAdmin = false;
-        //$admin = \App\Models\Administrador::where('user_id', $user->id)->where('activo', true)->first();
-        //if ($admin) { $isAdmin = true; }
+        $user = User::where('email', $dummyUser['email'])->first();
 
-        info('Usuario logueado', ['id' => $user->id, 'is_admin' => $user->is_admin]);
-        
-        //$user->is_admin = $isAdmin;
-        Auth::login($user);
-        return redirect('/home')->with('success', 'Login de debug exitoso.');
+        if (!$user) {
+            info('[Error] Usuario no logueado', ['email' => $dummyUser['email']]);
+            return redirect()->route('login')->with('error', 'Error autenticando con Google.');
+        }else{
+            info('Usuario logueado', ['id' => $user->id, 'is_admin' => $user->is_admin]);
+            //
+            Auth::login($user);
+            return redirect()->route('home')->with('success', 'Login de debug exitoso.');
+        }
     }
 
     public function redirectToGoogle()
@@ -57,26 +51,27 @@ class GoogleController extends Controller
     {
         try {
             $googleUser = Socialite::driver('google')->user();
-            $user = User::firstOrCreate([
-                'email' => $googleUser->getEmail(),
-            ], [
-                'name' => $googleUser->getName(),
-                'google_id' => $googleUser->getId(),
-                'avatar' => $googleUser->getAvatar(),
-                'password' => bcrypt('dummy_password'),
-                "is_admin" => false
-            ]);
+            info('GoogleUser logueado: ' . json_encode($googleUser));
+            // Buscar usuario solo por email, asegurando que sea único
+            $user = User::where('email', $googleUser->getEmail())->first();
 
-            // Validar si el usuario es administrador y está activo
-            $isAdmin = false;
-            $admin = \App\Models\Administrador::where('user_id', $user->id)->where('activo', true)->first();
-            if ($admin) { $isAdmin = true; }
-            
-            $user->is_admin = $isAdmin;
-            Auth::login($user);
-            return redirect('/home');
+            if (!$user) {
+                info('[Error] Usuario no logueado', $googleUser->getEmail());
+                return redirect()->route('login')->with('error', 'Error autenticando con Google.');
+            }else{
+                $user->google_id = $googleUser->getId();
+                $user->avatar = $googleUser->getAvatar();
+                $user->save();
+
+                info('Usuario logueado', ['id' => $user->id, 'is_admin' => $user->is_admin]);
+                //
+                Auth::login($user);
+                return redirect()->route('home')->with('success', 'Login de debug exitoso.');
+            }
+
         } catch (\Exception $e) {
-            return redirect('/login')->with('error', 'Error autenticando con Google.');
+            info('[Error] Usuario no logueado', ['error' => $e->getMessage()]);
+            return redirect()->route('login')->with('error', 'Error autenticando con Google.');
         }
     }
 }
